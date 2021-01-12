7be28eabf241e72cdc69df77fdbe830418f39787
nikothcdmt <76887650+nikothcdmt@users.noreply.github.com>
2021-01-11 23:58:31 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 const express = require('express')
 const app = express()
- 
+var bodyParser = require("body-parser")
+const mongoose = require('mongoose');
+
+
+
+
+app.use(bodyParser.json())
+
+
+mongoose.connect('mongodb://localhost/basic_whitelist', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+
+var userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello, for all HUmankind')
 })
  
+
+
+
+// POST /api/v1/signup
+app.post("/api/v1/signup", function(req,res){
+  if(req.body.username.length == 0 || req.body.password.length == null){
+    res.status(400).send({error:"username/password cannot be empty"})
+  }else{
+    
+  }
+})
+
+
+
 app.listen(3000)
